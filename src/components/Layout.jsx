677bcf2888c2069f46606cb32d1e6600f7bdd936
ojea <rnjsdhwo1325@gmail.com>
2021-12-19 23:56:02 +0900

    211219

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { FaTwitter, FaFacebookSquare, FaBsInstagram } from "react-icons/fa";
+import {
+  FaTwitter,
+  FaFacebookSquare,
+  FaInstagram,
+  FaLeaf,
+} from "react-icons/fa";
 import "../css/Header.css";
 import "../css/Footer.css";
 const Layout = ({ children }) => {
@@ -9,25 +14,53 @@ const Layout = ({ children }) => {
         <h3 style={{ color: "white" }}>헤더임</h3>
       </header>
       {children}
-      <footer style={{ color: "white" }}>
-        footer여기있다
-        <ul>
+      <footer>
+        <ul className="logo">
           <li>
-            <a className="tw" href="https://twitter.com/bts_bighit?lang=ko">
-              <FaTwitter color="1da1f2" size="25" />
+            <a
+              href="https://twitter.com/bts_bighit?lang=ko"
+              // 새창열림  target="_blank"   rel="noopener noreferrer"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter className="tw" />
             </a>
           </li>
           <li>
-            <a href="https://ko-kr.facebook.com/">
-              <FaFacebookSquare />
+            <a
+              href="https://ko-kr.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookSquare className="fb" />
             </a>
           </li>
           <li>
-            <a href="https://www.instagram.com/?hl=ko">
-              {/* <FaBsInstagram /> */}
+            <a
+              href="https://www.instagram.com/?hl=ko"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="star" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://sesac.seoul.kr/common/greeting.do"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLeaf className="leaf" />
             </a>
           </li>
         </ul>
+        <div className="copy">
+          <p>Copyright &copy; SeSAc Namu.Wiki </p>
+          <p style={{ fontSize: "12px" }}>
+            This site is protected by reCAPTCHA and the Google Privacy Policy
+            and Terms of Service apply.
+          </p>
+        </div>
       </footer>
     </div>
   );
